584ea2c39cee6a13f4f8bfe34acaf30fa24e33b0
Abdelrahman Akmal <73594338+AbdelrahmanAkmal@users.noreply.github.com>
2023-10-17 10:33:40 +0300

    Update generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 import streamlit_authenticator as stauth
 
-names = ['Akmal','Menna']
-usernames = ['Akmal','Menna']
-passwords = ['abc123','abc123']
+names = ['Akmal','Menna','Karim']
+usernames = ['Akmal','Menna','Karim']
+passwords = ['abc123','abc123','abc123']
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
